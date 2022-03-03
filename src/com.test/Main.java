84895fdcce6590edd18a988f57384a0b9d3c28b2
verdaux <xachineon@gmail.com>
2022-03-03 20:35:28 +0530

    Initial commit

---
+++ src/com.test/Main.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Main {
+    static int myVar;
+    public static void main(String[] args)
+    {
+        float floatVal=1.0f;
+        double doubleVal=4.0d;
+        byte byteVal=7;
+        short shortVal=7;
+        long longVal=5;
+
+        short result = byteVal;
+        System.out.println("Success");
+    }
+}
